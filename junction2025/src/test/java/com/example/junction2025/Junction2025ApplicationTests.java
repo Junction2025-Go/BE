66bfbd34c54030ce0962aaf0ebd4ee88ea7ca3d3
keon22han <keonheehan@hansung.ac.kr>
2025-11-11 12:08:39 +0900

    feat: 프로젝트 초기 생성
    프로젝트 초기 생성

---
+++ junction2025/src/test/java/com/example/junction2025/Junction2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junction2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junction2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
